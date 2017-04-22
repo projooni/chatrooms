b738a5568d6be03a6573f1b1b018ada9f586c1a3
projooni@gmail.com <ljs79337A@>
2017-04-23 00:20:30 +0900

    Socket.IO 서버 시작 로직 추가

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var fs = require('fs');
 var path = require('path');
 var mime = require('mime');
 var cache = {};
+var chatServer = require('./lib/chat_server');
 
 function send404(response){
     response.writeHead(404,{'Content-Type': 'text/plain'});
@@ -56,3 +57,6 @@ var server = http.createServer(function(request, response){
 server.listen(3000, function(){
    console.log('Server listening on port 3000.');
 });
+
+chatServer.listen(server);
+
